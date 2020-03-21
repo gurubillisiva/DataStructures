a2fa0d64bff0679482c5ad1784da945c973ce83e
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-21 11:46:16 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,18 +1,19 @@
 
-arr = [64, 25, 12, 22, 11]
+def selection_sort(arr):
+    for i in range(0,len(arr)):
+        min_indx = i
+
+        for j in range(i+1,len(arr)):
+            if arr[min_indx] > arr[j]:
+                min_indx = j
+        temp = arr[min_indx]
+        arr[min_indx] = arr[i]
+        arr[i] = temp
+
+arr1 = [64, 25, 12, 22, 11]
 print("Before sorting")
-print(arr)
-for i in range(0,len(arr)):
-    min_indx = i
-
-    for j in range(i+1,len(arr)):
-        if arr[min_indx] > arr[j]:
-            min_indx = j
-    temp = arr[min_indx]
-    arr[min_indx] = arr[i]
-    arr[i] = temp
-
-print("Sorted array", arr)
-selectionSort(arr)
-for i in range(len(arr)):
-    print("%d" %arr[i]),
+print(arr1)
+selection_sort(arr1)
+print("Sorted array", arr1)
+for i in range(len(arr1)):
+    print("%d" %arr1[i]),
